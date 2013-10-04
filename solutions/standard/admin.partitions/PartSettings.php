d4a14567c51596289165701b69619d66d2aadf14
Student Ivan <cryptox3@gmail.com>
2013-10-04 15:09:09 +0400

    rc 25 zero

--- solutions/standard/admin.partitions/PartSettings.php
+++ solutions/standard/admin.partitions/PartSettings.php
@@ -10,7 +10,13 @@ class PartSettings extends PartObjectInterface
 
     public function main()
     {
-        Process::$context['settings'] = JSONConfig::read('dynamical.json');
+        Process::$context['settings'] = array(
+            'd' => JSONConfig::read('dynamical.json'),
+            'm' => JSONConfig::read('mailer.json'),
+            'n' => file_get_contents(dirname(__FILE__) . "/../../../config/navigation.json"),
+            'a' => file_get_contents(dirname(__FILE__) . "/../../../config/admin.json"),
+            'b' => file_get_contents(dirname(__FILE__) . "/../../../config/base.json"),
+        );
         Process::$context['roles_list'] = Roles::listing();
         return Process::getTwigInstance()->render('admin/partitions/configuration.html.twig', Process::$context);
     }
@@ -47,10 +53,68 @@ class PartSettings extends PartObjectInterface
         /**
          * КОСТЫЛЬ
          */
-        $_POST['input__session__garbage_auto_dump'] = isset($_POST['input__session__garbage_auto_dump']);
-        $_POST['input__cms__email_confirm'] = isset($_POST['input__cms__email_confirm']);
-        $_POST['input__cms__turn_on_registration'] = isset($_POST['input__cms__turn_on_registration']);
-        $_POST['input__cms__visitors'] = isset($_POST['input__cms__visitors']);
+        switch ($postConfigFile) {
+            case 'dynamical':
+                $_POST['input__session__garbage_auto_dump'] = isset($_POST['input__session__garbage_auto_dump']);
+                $_POST['input__cms__email_confirm'] = isset($_POST['input__cms__email_confirm']);
+                $_POST['input__cms__turn_on_registration'] = isset($_POST['input__cms__turn_on_registration']);
+                $_POST['input__cms__visitors'] = isset($_POST['input__cms__visitors']);
+                break;
+            case 'mailer':
+                $_POST['input__smtp__encryption'] = isset($_POST['input__smtp__encryption']) ?
+                    (
+                        (
+                            (strtolower($_POST['input__smtp__encryption']) === 'null') ?
+                                null : $_POST['input__smtp__encryption']
+                        )
+                    ) : null;
+                if (isset($_POST['input__smtp__encryption']) and !$_POST['input__smtp__encryption']) {
+                    $_POST['input__smtp__encryption'] = null;
+                }
+                break;
+            case 'navigation':
+                if ($tmpCfg = Data::input('navigation_json_raw')) {
+                    if (JSONConfig::save("$postConfigFile.json", json_decode($tmpCfg))) {
+                        Process::$context['flash_success'] = 'Настройки сохранены.';
+                        return $this->main();
+                    } else {
+                        Process::$context['flash_danger'] = 'Произошла ошибка при компиляции настроек.';
+                        return Process::getTwigInstance()->render('admin/alert.danger.html.twig', Process::$context);
+                    }
+                } else {
+                    Process::$context['flash_danger'] = 'Произошла ошибка при компиляции настроек.';
+                    return Process::getTwigInstance()->render('admin/alert.danger.html.twig', Process::$context);
+                }
+                break;
+            case 'admin':
+                if ($tmpCfg = Data::input('admin_json_raw')) {
+                    if (JSONConfig::save("$postConfigFile.json", json_decode($tmpCfg))) {
+                        Process::$context['flash_success'] = 'Настройки сохранены.';
+                        return $this->main();
+                    } else {
+                        Process::$context['flash_danger'] = 'Произошла ошибка при компиляции настроек.';
+                        return Process::getTwigInstance()->render('admin/alert.danger.html.twig', Process::$context);
+                    }
+                } else {
+                    Process::$context['flash_danger'] = 'Произошла ошибка при компиляции настроек.';
+                    return Process::getTwigInstance()->render('admin/alert.danger.html.twig', Process::$context);
+                }
+                break;
+            case 'base':
+                if ($tmpCfg = Data::input('base_json_raw')) {
+                    if (JSONConfig::save("$postConfigFile.json", json_decode($tmpCfg))) {
+                        Process::$context['flash_success'] = 'Настройки сохранены.';
+                        return $this->main();
+                    } else {
+                        Process::$context['flash_danger'] = 'Произошла ошибка при компиляции настроек.';
+                        return Process::getTwigInstance()->render('admin/alert.danger.html.twig', Process::$context);
+                    }
+                } else {
+                    Process::$context['flash_danger'] = 'Произошла ошибка при компиляции настроек.';
+                    return Process::getTwigInstance()->render('admin/alert.danger.html.twig', Process::$context);
+                }
+                break;
+        }
 
         foreach (array_keys($_POST) as $key) {
             if (substr($key, 0, 6) === 'input_') {
